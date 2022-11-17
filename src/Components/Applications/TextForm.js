484a2ad457fb9128c315fcb5749600cba025b823
Haris <azan6743@gmail.com>
2022-11-17 15:31:40 +0500

    Text to Uppercase letters3

--- src/Components/Applications/TextForm.js
+++ src/Components/Applications/TextForm.js
@@ -38,8 +38,8 @@ export default function TextForm() {
         <div className="row">
           <div className="col">
             <h3>Words and Letters</h3>
-            <p>{text.split(" ").length} words and {text.length} letters</p>
-            <h3>Time taking</h3>
+            <p>{text.split(" ").length-1} words and {text.length} letters</p>
+            <h3>Time taking per Words</h3>
             <p>taking {0.08*text.split(" ").length}</p>
             <h3>Preview</h3>
             <p>{text}</p>
